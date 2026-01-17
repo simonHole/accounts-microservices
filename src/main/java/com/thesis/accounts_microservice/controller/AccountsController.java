ea18bdb62dfb75393db4aa4a86c50354da3284e2
Szymon Dziura <dziura.biurowe@gmail.com>
2026-01-17 13:41:37 +0100

    add documentation for AccountsController in Swagger (not completed)

--- src/main/java/com/thesis/accounts_microservice/controller/AccountsController.java
+++ src/main/java/com/thesis/accounts_microservice/controller/AccountsController.java
@@ -6,6 +6,8 @@ import com.thesis.accounts_microservice.dto.CustomerDTO;
 import com.thesis.accounts_microservice.dto.ResponseDTO;
 import com.thesis.accounts_microservice.service.IAccountsService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
@@ -37,6 +39,10 @@ public class AccountsController {
             summary = "Create new account CREATE REST API endpoint",
             description = "Create new account with bounded customer by send data only basic data"
     )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status Created"
+    )
     @PostMapping(path = "/create-account")
     public ResponseEntity<ResponseDTO> createAccount(@Valid @RequestBody CustomerDTO customerDTO) {
 
@@ -50,6 +56,10 @@ public class AccountsController {
     @Operation(
             summary = "Find existing account READ REST API endpoint",
             description = "Find and return customer with account if exists in database."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status OK"
     )
     @GetMapping("/fetch-customer")
     public ResponseEntity<CustomerDTO> fetchAccountDetails(@RequestParam
@@ -67,6 +77,16 @@ public class AccountsController {
             summary = "Update whole existing account PUT REST API endpoint",
             description = "Update all data about particular user based on its mobile phone number"
     )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error"
+            )
+    })
     @PutMapping("/update-customer")
     public ResponseEntity<ResponseDTO> updateAccount(@Valid @RequestBody CustomerDTO customerDTO) {
         boolean isUpdated = accountsService.updateAccount(customerDTO);
@@ -81,6 +101,20 @@ public class AccountsController {
         }
     }
 
+    @Operation(
+            summary = "Delete existing account DELETE REST API endpoint",
+            description = "Delete existing customer and account based on its mobile phone number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error"
+            )
+    })
     @DeleteMapping("/delete-customer")
     public ResponseEntity<ResponseDTO> deleteAccount(@RequestParam
                                                          @Pattern(regexp = "(^$|[0-9]{9})",
